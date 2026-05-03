0be39f95b8efc921eaa6862c8740d89b3cc7664d
Rayna <raynaldgitau@gmail.com>
2026-05-03 12:50:43 +0300

    dashboard works

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "drizzle-kit";
-import path from "path";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
 
 export default defineConfig({
-  schema: path.join(__dirname, "./src/schema/index.ts"),
+  schema: "./src/index.ts", // Point this exactly to where your pgTable definitions are
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
