b19158fb12071ef65d5bd4e7af94f5777e48f597
Rayna <raynaldgitau@gmail.com>
2026-05-03 07:31:55 +0300

    fixes5

--- artifacts/portfolio/src/main.tsx
+++ artifacts/portfolio/src/main.tsx
@@ -1,5 +1,8 @@
+import { setBaseUrl } from '@workspace/api-client-react';
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+setBaseUrl(import.meta.env.VITE_API_URL);
 
 createRoot(document.getElementById("root")!).render(<App />);
